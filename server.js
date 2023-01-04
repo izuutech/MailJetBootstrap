53a4f57b8b04699f58e03bfacd48b72426953770
izuutech <izutechsofficial@gmail.com>
2023-01-04 14:11:20 +0100

    initial

---
+++ server.js
@@ -0,0 +1,29 @@
+const mailjet = require("node-mailjet").connect("", "");
+const request = mailjet.post("send", { version: "v3.1" }).request({
+  Messages: [
+    {
+      From: {
+        Email: "youremail@gmail.com",
+        Name: "Sender Email",
+      },
+      To: [
+        {
+          Email: "youremail@gmail.com",
+          Name: "Sender Email",
+        },
+      ],
+      Subject: "Greetings from Mailjet.",
+      TextPart: "My first Mailjet email",
+      HTMLPart:
+        "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+      CustomID: "AppGettingStartedTest",
+    },
+  ],
+});
+request
+  .then((result) => {
+    console.log(result.body);
+  })
+  .catch((err) => {
+    console.log(err.statusCode);
+  });
